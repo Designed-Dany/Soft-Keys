50cd792b8ef991beae0a45bfcdcb8e899fc16a81
Danila Udalchikov <githubs69@gmail.com>
2025-04-11 11:07:05 +0300

    feat: search products and add Context

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "/src/scss/_sort.scss";
 const filters = [
-  { name: "Популярности", sortProperty: "rating" },
-  { name: "Цене", sortProperty: "price" },
+  { name: "Популярности(Выше)", sortProperty: "rating" },
+  { name: "Популярности(Ниже)", sortProperty: "-rating" },
+  { name: "Цене(Дороже)", sortProperty: "price" },
+  { name: "Цене(Дешевле)", sortProperty: "-price" },
 ];
 function Sort({ value, onChangeSort }) {
   const [open, setOpen] = React.useState(false);
@@ -21,7 +23,11 @@ function Sort({ value, onChangeSort }) {
             {filters.map((obj, i) => (
               <li
                 onClick={() => (onChangeSort(obj), setOpen(false))}
-                className={value === i ? "filters__active" : "filters__item"}
+                className={
+                  value.sortProperty === obj.sortProperty
+                    ? "filters__active"
+                    : "filters__item"
+                }
                 key={i}
               >
                 {obj.name}
