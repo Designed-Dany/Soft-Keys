f28a72986a865ec2d350879192c47bebf197c522
Мой компухтер <lena@Danil.local>
2025-06-02 22:04:58 +0300

    fix: saving the style of the active favorites button

---
+++ src/components/Keyboard/FavoriteBtn.jsx
@@ -0,0 +1,8 @@
+
+function FavoriteBtn() {
+	return (
+		
+	)
+}
+
+export default FavoriteBtn;
--- src/components/Keyboard/index.jsx
+++ src/components/Keyboard/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   addFavorites,
@@ -6,12 +6,20 @@ import {
 } from "../../redux/slices/favoriteSlice";
 import "/src/scss/_keyboard.scss";
 
-function Keyboard({ id, title, price, imageUrl, format }) {
+function Keyboard({ id, title, price, imageUrl, format, isFav}) {
   const dispatch = useDispatch();
   const favorites = useSelector((state) => state.favorites.items);
-  let isFavorite;
+  const [isActive, setIsActive] = React.useState(isFav)
+  
+  let isFavorite
 
-  const onClickAdd = () => {
+  useEffect(() => {
+      isFavorite = favorites.some((item) => item.id === id);
+      setIsActive(isFavorite)
+  }, [favorites, id]);
+
+ const onClickAdd = () => {
+
     const items = {
       id,
       title,
@@ -20,12 +28,8 @@ function Keyboard({ id, title, price, imageUrl, format }) {
       format,
     };
 
-    console.log(favorites);
-
-    isFavorite = favorites.some((item) => item.id === items.id);
-
-    if (isFavorite) {
-      dispatch(removeFavorites(items.id));
+      if (isActive) {
+      dispatch(removeFavorites(id));
     } else {
       dispatch(addFavorites(items));
     }
@@ -51,11 +55,11 @@ function Keyboard({ id, title, price, imageUrl, format }) {
           width="28"
           height="24"
           viewBox="0 0 28 24"
-          fill="red"
+          fill={isActive ? 'red' : 'white'}
         >
           <path
             d="M19.25 3C17.22 3 15.2716 3.81 14 5.085C12.7283 3.81 10.78 3 8.74998 3C5.15081 3 2.33331 5.415 2.33331 8.5C2.33331 12.275 6.29998 15.36 12.3083 20.035L14 21.35L15.6916 20.035C21.7 15.36 25.6666 12.275 25.6666 8.5C25.6666 5.415 22.8491 3 19.25 3ZM14.1225 18.555L14 18.65L13.8775 18.555C8.32998 14.24 4.66665 11.39 4.66665 8.5C4.66665 6.505 6.42248 5 8.74998 5C10.5466 5 12.2966 5.995 12.9091 7.36H15.085C15.7033 5.995 17.4533 5 19.25 5C21.5775 5 23.3333 6.505 23.3333 8.5C23.3333 11.39 19.67 14.24 14.1225 18.555Z"
-            fill="red"
+            fill={isActive ? 'red' : 'white'}
           />
         </svg>
       </button>
